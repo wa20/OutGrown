c5d73bb8ca82c668accd30909b52dbaf5bf4e4e0
wael <78425538+wa20@users.noreply.github.com>
2021-08-03 09:37:31 +0100

    minor code amends

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { render } from 'react-dom';
 import {
   ApolloClient,
   InMemoryCache,
@@ -8,15 +9,9 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
+
+import Navbar from './components/navbar/navbar.js';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -40,24 +35,18 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+
       <Router>
-        <div>
-          <StoreProvider>
-            <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/success" component={Success} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
-            </Switch>
-          </StoreProvider>
-        </div>
+        <Navbar/>
+        {/* <Route exact path="/" component={Home} /> */}
+
+        {/* <Route component={Navbar}/> */}
+       
+
       </Router>
     </ApolloProvider>
   );
 }
 
 export default App;
+render(<App />, document.getElementById('root'));
