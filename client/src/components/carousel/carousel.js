c27c39bc7e4bc9bdb6a8cf8614c8e9356710e007
wael <78425538+wa20@users.noreply.github.com>
2021-08-06 12:36:35 +0100

    carousel ammends and added carousel css

--- client/src/components/carousel/carousel.js
+++ client/src/components/carousel/carousel.js
@@ -5,21 +5,22 @@ import Stock1 from '../../assets/stock1.jpg'
 import Stock2 from '../../assets/stock2.jpg'
 import Stock3 from '../../assets/stock3.jpg'
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+// import Card from '@material-ui/core/Card';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
 import { Segment, Image } from 'semantic-ui-react'
+import "./carousel.css"
 
 const useStyles = makeStyles({
     root: {
       maxWidth: 345,
     },
     media: {
-      height: 140,
+      height: 100,
     },
   });
 
@@ -33,18 +34,18 @@ export default function DefaultCarousel() {
       };
 
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect} fade>
-            <Carousel.Item>
+        <Carousel activeIndex={index} onSelect={handleSelect} fade  style={{ padding: '1em 0em' }}>
+            <Carousel.Item style={{height:"50vh"}}>
                 <Segment size='small'>
                     <Image src={Stock1} centered className="d-block w-100"/>
                 </Segment>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={{height:"50vh"}}>
                 <Segment size='small'>
                     <Image src={Stock2}  centered className="d-block w-100"/>
                 </Segment>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={{height:"50vh" }}>
                 <Segment size='small'>
                     <Image src={Stock3}  centered className="d-block w-100"/>
                 </Segment>
