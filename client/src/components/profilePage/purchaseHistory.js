0cfdf89a94f858d25246ec6e4a5d98a9b13ecf37
wael <78425538+wa20@users.noreply.github.com>
2021-08-09 17:44:54 +0100

    purchase history page added

---
+++ client/src/components/profilePage/purchaseHistory.js
@@ -0,0 +1,61 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// import { useQuery } from '@apollo/client';
+// import { QUERY_USER } from '../utils/queries';
+
+import { createMedia } from "@artsy/fresnel";
+// import PropTypes from "prop-types";
+// import { useMutation } from '@apollo/client';
+// import Auth from '../../utils/auth';
+// import { ADD_USER } from '../../utils/mutations';
+
+import {
+  Button,
+  Card,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Sidebar,
+  Visibility,
+  Accordion,
+  Form,
+  Table,
+  Tab
+} from "semantic-ui-react";
+
+
+
+
+function PurchaseHistory() {
+
+
+// const [state, dispatch] = useStoreContext();
+
+//   const {
+//     image,
+//     name,
+//     _id,
+//     price,
+//     description,
+//   } = item;
+
+  return (
+    <>
+      <div className="container my-1">
+          
+     
+
+        
+      </div>
+    </>
+  );
+}
+
+export default PurchaseHistory;
