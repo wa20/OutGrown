acffa6abd1d60a8e57484181534163a58973552e
wael <78425538+wa20@users.noreply.github.com>
2021-08-09 17:43:06 +0100

    listing page draft created

---
+++ client/src/components/profilePage/listings.js
@@ -0,0 +1,72 @@
+import { createMedia } from "@artsy/fresnel";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import "./profilePage.css"
+
+// import Button from '@material-ui/core/Button';
+
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Sidebar,
+  Visibility,
+  Accordion,
+  Form,
+  Table
+} from "semantic-ui-react";
+
+
+
+const Listings = () => (
+    <div>
+
+    <Segment style={{ padding: '5em 0em' }} vertical textAlign="center">
+
+
+            
+    </Segment>
+    
+
+    </div>
+
+)
+
+export default Listings
+
+
+
+// const panes = [
+//   {
+//     menuItem: "Profile",
+//     render: () => (
+//       <Tab.Pane>
+//         <Segment style={{ padding: "1em 0em" }} vertical textAlign="center">
+//           <Container text>
+//             <Header style={{ fontSize: "1.8em" }}>Hi 'user name'</Header>
+//             {/* <Container text > */}
+//             <p style={{ fontSize: "1.2em" }}>29 Fake Street</p>
+//             <p style={{ fontSize: "1.2em" }}>Fake Side City</p>
+//             <p style={{ fontSize: "1.2em" }}>FK3 9SD</p>
+//             {/* </Container> */}
+//           </Container>
+//         </Segment>
+//       </Tab.Pane>
+//     ),
+//   },
+
+//   {
+//     menuItem: "Edit Profile",
+//     render: () => <Tab.Pane >
+    
+                     
+//     </Tab.Pane>,
+//   },
+// ];
