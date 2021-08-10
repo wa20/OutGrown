c51d2ac2e89efe45fe494e9ae690c1cda39b29b3
wael <78425538+wa20@users.noreply.github.com>
2021-08-10 12:32:20 +0100

    user details added to queries and mutations. also more details added to seed file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -29,3 +29,27 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const UPDATE_USER = gql`
+  mutation updateUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $username: String!
+    $street: String!
+    $townCity: String!
+    $postCode: String!
+
+  ) {
+    updateUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+      username: $username
+      street: $street
+      townCity: $townCity
+      postCode: $postCode
+    ) 
+  }
+`;
