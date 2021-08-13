6b74b1cbe5ec57a6a6222d8afb3b758e96c6c053
wael <78425538+wa20@users.noreply.github.com>
2021-08-13 18:07:54 +0100

    commented out code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-require("dotenv").config()
-console.log("Hello", process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/outgrown', {
+//  require("dotenv").config()
+
+mongoose.connect(
+  process.env.MONGODB_URI || 
+  'mongodb://localhost/outgrown', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
-
+// console.log("Hello", process.env.MONGODB_URI)
 mongoose.connection.on('connected', () => {
   console.log('Mongoose Connected')
 })
