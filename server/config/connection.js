46ec86d3e1f117cadda4b0d4903e7a1d2570aed3
Freddie Brewin <freddiebrewin@live.com>
2021-08-13 17:33:27 +0100

    Fixing profile data rendering

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-require("dotenv").config()
-console.log("Hello", process.env.MONGODB_URI)
+/* require("dotenv").config()
+console.log("Hello", process.env.MONGODB_URI) */
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/outgrown', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
